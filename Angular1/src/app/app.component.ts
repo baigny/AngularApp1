b93f1b604c0b0e41207729c541ab1b22656f6e70
baigny <baigny@gmail.com>
2017-12-08 22:02:15 +0530

    blank

--- Angular1/src/app/app.component.ts
+++ Angular1/src/app/app.component.ts
@@ -1,10 +1,3 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
 export class AppComponent {
-  title = 'app';
+  title = 'Angular Application Building Foundation';
 }
